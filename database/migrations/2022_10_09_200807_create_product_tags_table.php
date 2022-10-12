64b4c40853f478bc217a633d7940955b3f9a79cd
Kaio F. G. Lazaro <kaiolazaro36@hotmail.com>
2022-10-12 02:50:16 -0300

    Fix: Add id column to product tags

--- database/migrations/2022_10_09_200807_create_product_tags_table.php
+++ database/migrations/2022_10_09_200807_create_product_tags_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('product_tags', function (Blueprint $table) {
+            $table->id();
             $table->foreignId('product_id')->constrained('products');
             $table->foreignId('tag_id')->constrained('tags');
         });
